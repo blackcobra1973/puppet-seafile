aa561bcaf7002ece2e6b0a31140ed4b483f1ce10
Kurt Dillen <kurt.dillen@dls-belgium.com>
2014-12-17 15:23:07 +0100

    * Adding auto mysql/mariadb configuration. [SYS-380]

---
+++ manifests/mysql.pp
@@ -0,0 +1,33 @@
+# = Class: seafile::mysql
+#
+# Install and configure MySQL/MariaDB
+#
+class seafile::mysql
+{
+  if $seafile::use_mysql
+  {
+  #  include mysql::server
+
+    $override_options = hiera_hash('mysql::override_options')
+
+    if $override_options {
+      class { 'mysql::server':
+        override_options  => $override_options,
+        require           => [Package['mysql_client']]
+      }
+    } else {
+      # we don't use default options, so error if nothing is found
+      fail('No hash found to override settings in profile::mysql.')
+    }
+
+    ###  Create Databases + users + grants
+    $dbs = hiera_hash('mysql::databases')
+    $users = hiera_hash('mysql::users')
+    $grants = hiera_hash('mysql::grants')
+
+    create_resources('mysql_database', $dbs)
+    create_resources('mysql_user', $dbs)
+    create_resources('mysql_grant', $dbs)
+
+  }
+}
