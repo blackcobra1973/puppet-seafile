4e5e5b3a6ee69c26e141dda6d89d8e94b2b4a488
Kurt Dillen <kurt.dillen@dls-belgium.com>
2014-12-17 16:59:05 +0100

    * Fixes

--- manifests/unpack.pp
+++ manifests/unpack.pp
@@ -12,35 +12,53 @@ class seafile::unpack(
   {
     ### Unpack the seafile server
     exec { 'untar-seafile':
-      command     => "tar xzf ${seafile::base_dir}/installed/seafile-pro-server_${seafile::version}_x86-64.tar.gz",
+      command     => "/bin/tar xzf ${seafile::base_dir}/installed/seafile-pro-server_${seafile::version}_x86-64.tar.gz",
       cwd         => $seafile::base_dir,
-      creates     => "${seafile::base_dir}/seafile-pro-server_${seafile::version}",
+      creates     => "${seafile::base_dir}/seafile-pro-server-${seafile::version}",
       subscribe   => File['seafile_source'],
       refreshonly => true,
+      user        => 'root',
     }
 
+    ### Set file ownership
+#    exec { 'set_correct_user':
+#      command     => "/bin/chown -R root:root ${seafile::base_dir}/seafile-pro-server-${seafile::version}",
+#      require     => Exec['untar-seafile'],
+#      refreshonly => true,
+#      user        => 'root',
+#    }
+
     ### Symlink the directory
-    file { "${seafile::base_dir}/seafile-pro-server_latest":
-      ensure  => "${seafile::base_dir}/seafile-pro-server_${seafile::version}",
-      require => Exec['untar-seafile'];
-    }
+#    file { "${seafile::base_dir}/seafile-pro-server-latest":
+#      ensure  => "${seafile::base_dir}/seafile-pro-server-${seafile::version}",
+#      require => Exec['untar-seafile'];
+#    }
   }
   else
   {
     ### Unpack the seafile server
     exec { 'untar-seafile':
-      command     => "tar xzf ${seafile::base_dir}/installed/seafile-server_${seafile::version}_x86-64.tar.gz",
+      command     => "/bin/tar xzf ${seafile::base_dir}/installed/seafile-server_${seafile::version}_x86-64.tar.gz",
       cwd         => $seafile::base_dir,
-      creates     => "${seafile::base_dir}/seafile-server_${seafile::version}",
+      creates     => "${seafile::base_dir}/seafile-server-${seafile::version}",
       subscribe   => File['seafile_source'],
       refreshonly => true,
+      user        => 'root',
     }
 
+    ### Set file ownership
+#    exec { 'set_correct_user':
+#      command     => "/bin/chown -R root:root ${seafile::base_dir}/seafile-server-${seafile::version}",
+#      require     => Exec['untar-seafile'],
+#      refreshonly => true,
+#      user        => 'root',
+#    }
+
     ### Symlink the directory
-    file { "${seafile::base_dir}/seafile-server_latest":
-      ensure  => "${seafile::base_dir}/seafile-server_${seafile::version}",
-      require => Exec['untar-seafile'];
-    }
+#    file { "${seafile::base_dir}/seafile-server-latest":
+#      ensure  => "${seafile::base_dir}/seafile-server-${seafile::version}",
+#      require => Exec['untar-seafile'];
+#    }
   }
 
 }
